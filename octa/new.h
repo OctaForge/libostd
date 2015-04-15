dba2ac673f94cf3aabe8a9edfe7ad616c71de2e8
q66 <daniel@octaforge.org>
2015-04-15 01:39:26 +0100

    split new implementations

--- octa/new.h
+++ octa/new.h
@@ -1,32 +1,10 @@
-/* Default new/delete operator overloads for OctaSTD.
+/* Default new/delete operator overloads for OctaSTD. Also has an impl file.
  *
  * This file is part of OctaSTD. See COPYING.md for futher information.
  */
 
 #ifndef OCTA_NEW_H
 #define OCTA_NEW_H
-
-#include <stdlib.h>
-
-inline void *operator new(size_t size) {
-    void  *p = malloc(size);
-    if   (!p) abort();
-    return p;
-}
-
-inline void *operator new[](size_t size) {
-    void  *p = malloc(size);
-    if   (!p) abort();
-    return p;
-}
-
-inline void operator delete(void *p) {
-    free(p);
-}
-
-inline void operator delete[](void *p) {
-    free(p);
-}
 
 inline void *operator new     (size_t, void *p) { return p; }
 inline void *operator new   [](size_t, void *p) { return p; }
---
+++ src/new.cpp
@@ -0,0 +1,26 @@
+/* Default new/delete operator overloads for OctaSTD. Also has a header file.
+ *
+ * This file is part of OctaSTD. See COPYING.md for futher information.
+ */
+
+#include <stdlib.h>
+
+void *operator new(size_t size) noexcept {
+    void  *p = malloc(size);
+    if   (!p) abort();
+    return p;
+}
+
+void *operator new[](size_t size) noexcept {
+    void  *p = malloc(size);
+    if   (!p) abort();
+    return p;
+}
+
+void operator delete(void *p) noexcept {
+    free(p);
+}
+
+void operator delete[](void *p) noexcept {
+    free(p);
+}
