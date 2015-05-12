3e51a86aa4d8c2ee4b6a0cbcf39ba9305c2aaed6
q66 <daniel@octaforge.org>
2015-05-12 21:27:46 +0100

    add header file for stream (no implementation yet)

---
+++ octa/stream.h
@@ -0,0 +1,16 @@
+/* Generic stream implementation for OctaSTD.
+ *
+ * This file is part of OctaSTD. See COPYING.md for futher information.
+ */
+
+#ifndef OCTA_STREAM_H
+#define OCTA_STREAM_H
+
+namespace octa {
+    struct stream {
+        stream() {}
+        virtual ~stream() {}
+    };
+}
+
+#endif
