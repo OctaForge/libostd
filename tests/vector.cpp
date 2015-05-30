a33eb18ec36a07f2784578ec932200d0e5933f25
q66 <daniel@octaforge.org>
2015-05-30 02:57:35 +0100

    initial example vector tests

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -1,5 +1,24 @@
+#include <assert.h>
 #include "octa/vector.h"
 
+using namespace octa;
+
 int main() {
+    Vector<int> x = { 5, 10, 15, 20 };
+
+    assert(x.first() == 5);
+    assert(x.last() == 20);
+
+    assert(x[0] == 5);
+    assert(x[2] == 15);
+
+    assert(x.size() == 4);
+
+    Vector<int> y(5, 10);
+
+    assert(y.size() == 5);
+    assert(y.first() == 10);
+    assert(y.last() == 10);
+
     return 0;
 }
