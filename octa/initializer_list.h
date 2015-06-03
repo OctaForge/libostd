e599a7f67a18ba48fb3aee651c384e96cf56eafd
q66 <daniel@octaforge.org>
2015-06-03 22:56:01 +0100

    deassify initializer_list

--- octa/initializer_list.h
+++ octa/initializer_list.h
@@ -13,32 +13,36 @@
 #ifndef OCTA_ALLOW_CXXSTD
 /* must be in std namespace otherwise the compiler won't know about it */
 namespace std {
-    template<typename _T>
-    class initializer_list {
-        const _T *__buf;
-        size_t __len;
 
-        initializer_list(const _T *__v, size_t __n): __buf(__v), __len(__n) {}
-    public:
-        initializer_list(): __buf(nullptr), __len(0) {}
+template<typename _T>
+class initializer_list {
+    const _T *p_buf;
+    size_t p_len;
 
-        size_t size() const { return __len; }
+    initializer_list(const _T *v, size_t n): p_buf(v), p_len(n) {}
+public:
+    initializer_list(): p_buf(nullptr), p_len(0) {}
 
-        const _T *begin() const { return __buf; }
-        const _T *end() const { return __buf + __len; }
-    };
+    size_t size() const { return p_len; }
+
+    const _T *begin() const { return p_buf; }
+    const _T *end() const { return p_buf + p_len; }
+};
+
 }
 #else
 #include <initializer_list>
 #endif
 
 namespace octa {
-    template<typename _T> using InitializerList = std::initializer_list<_T>;
 
-    template<typename _T>
-    octa::PointerRange<const _T> each(std::initializer_list<_T> __init) {
-        return octa::PointerRange<const _T>(__init.begin(), __init.end());
-    }
+template<typename _T> using InitializerList = std::initializer_list<_T>;
+
+template<typename _T>
+octa::PointerRange<const _T> each(std::initializer_list<_T> init) {
+    return octa::PointerRange<const _T>(init.begin(), init.end());
+}
+
 }
 
 #endif
