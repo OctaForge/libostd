52c32ee16649d7cfc824adef81985300a2e66510
q66 <daniel@octaforge.org>
2016-03-28 00:01:09 +0100

    add extra prints to examples

--- examples/range.cc
+++ examples/range.cc
@@ -22,10 +22,12 @@ int main() {
         writeln(i);
 
     /* prints ABCDEF (ASCII 65, 66, 67, 68, 69, 70) */
+    writeln("string gen test");
     String s(map(range(6), [](int v) -> char { return v + 65; }));
     writeln(s);
 
     /* join a few ranges together - prints 11, 22, 33 ... 99 each on new line */
+    writeln("range join test");
     auto x = { 11, 22, 33 };
     auto y = { 44, 55, 66 };
     auto z = { 77, 88, 99 };
@@ -37,15 +39,18 @@ int main() {
      * {44, 55, 66}
      * {77, 88, 99}
      */
+    writeln("range chunk test");
     auto cr = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
     for (auto r: chunks(iter(cr), 3))
         writeln(r);
 
     /* {11, 44, 77}, {22, 55, 88}, {33, 66, 99} */
+    writeln("range zip test");
     for (auto v: zip(iter(x), iter(y), iter(z)))
         writeln(v);
 
     /* 2-tuple zip uses Pair */
+    writeln("2-tuple range zip");
     for (auto v: zip(iter({ 5, 10, 15, 20 }), iter({ 6, 11, 16, 21 })))
         writeln(v.first, ", ", v.second);
 }
