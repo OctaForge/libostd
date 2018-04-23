621bad7474b1b80e2601e9559472783c51e99000
q66 <daniel@octaforge.org>
2018-04-23 21:24:46 +0200

    forgot to add file

---
+++ src/build_make.cc
@@ -0,0 +1,15 @@
+/* Build system implementation bits.
+ *
+ * This file is part of libostd. See COPYING.md for futher information.
+ */
+
+#include "ostd/build/make.hh"
+
+namespace ostd {
+namespace build {
+
+/* place the vtable in here */
+make_task::~make_task() {}
+
+} /* namespace build */
+} /* namespace ostd */
